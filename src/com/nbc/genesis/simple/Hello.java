7eba7eb5684573e7645bed067f605ea90fc7585a
Chris Bellingham <bellingham@informz.com>
2016-01-13 06:55:26 -0500

    Initial commit

---
+++ src/com/nbc/genesis/simple/Hello.java
@@ -0,0 +1,13 @@
+package com.nbc.genesis.simple;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Bellingham
+ * Date: 1/13/16
+ */
+public class Hello {
+
+    public static void sayHello() {
+        System.out.println("Hello world");
+    }
+}
