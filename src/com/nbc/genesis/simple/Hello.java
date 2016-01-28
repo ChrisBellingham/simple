b6f3cc45a6a25c2bce9652b8e5cdccaa8e25f7c6
Chris Bellingham <bellingham@informz.com>
2016-01-28 11:46:51 -0500

    That's all.
    That's all.

--- src/com/nbc/genesis/simple/Hello.java
+++ src/com/nbc/genesis/simple/Hello.java
@@ -10,6 +10,6 @@ public class Hello {
     public static void sayHello() {
         System.out.println("Hello World");
         
-        System.out.println("That's all folks. Thanks for all the salmon");
+        System.out.println("Ibidy, ibidy, ibidy, that's all folks");
     }
 }
